2d368f52fadd023f5a4831509c4dff9cc055cb02
jfurdzik <88293361+jfurdzik@users.noreply.github.com>
2026-01-03 11:56:08 -0500

    add logo and settings button

--- third-time/src/pages/StopwatchPage.jsx
+++ third-time/src/pages/StopwatchPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import MyStopwatch from '../components/MyStopwatch/MyStopwatch';
 import styles from './StopwatchPage.module.css';
+import logo from '../assets/logo.png';
+import settings from '../assets/settings.png';
 
 export default function StopwatchPage() {
   return (
     <div className={styles.component}>
+      <img src={logo} className={styles.logo}/>
+      <button className={styles.settings}>
+        <img src={settings}/>
+      </button>
       <MyStopwatch/>
     </div>
   );
